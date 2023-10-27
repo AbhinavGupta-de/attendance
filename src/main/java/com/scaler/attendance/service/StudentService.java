9a35c7acaed4e3e4df584b83707d98d09a2072cc
Abhinav Gupta <Abhinav.gupta@outlook.de>
2023-10-28 00:31:35 +0530

    Test1: Finished API

---
+++ src/main/java/com/scaler/attendance/service/StudentService.java
@@ -0,0 +1,27 @@
+package com.scaler.attendance.service;
+
+import com.scaler.attendance.model.Student;
+import com.scaler.attendance.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    // Methods that we need
+    // 1) Get students with a particular ID
+    // 2) Add students to the database(future scope)
+
+    @Autowired
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Optional<Student> getStudent(String studentId) {
+        return studentRepository.findByStudentId(studentId);
+    }
+}
